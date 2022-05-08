5128ffe04f0e2a24380bba59f6b2d6a4d1e30ae1
maahedahs <72372121+maahedahs@users.noreply.github.com>
2022-05-08 16:53:40 -0700

    Add files via upload

---
+++ project.py
@@ -0,0 +1,24 @@
+
+# CS 4990 Project
+
+# returns shortest path from one node to all other nodes
+def dijkstras():
+    pass
+
+def closeness_centrality():
+    dijkstras()
+    pass
+
+# Press the green button in the gutter to run the script.
+if __name__ == '__main__':
+    # input from the network
+
+    closeness_centrality()
+
+    # send results back to processor 0
+
+    # processor 0 prints closeness centrality for all nodes to output.txt
+
+    # processor 0 prints nodes with top 5 centrality values to output.txt
+
+    # processor 0 prints average of all nodes' centrality values to output.txt

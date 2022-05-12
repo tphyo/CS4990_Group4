af2c6558689981587c9e41d50ed1ca6cbe1b3e6a
Thet Hlaing Phyo <91230275+tphyo@users.noreply.github.com>
2022-05-11 18:03:56 -0700

    Push code for dijstra

--- project.py
+++ project.py
@@ -3,10 +3,39 @@
 
 from mpi4py import MPI
 import numpy as np
+from queue import PriorityQueue
 
 # returns shortest path from one node to all other nodes
-def dijkstras(G, i):
-    pass
+def dijkstra(G, start, goal):
+    """ Uniform-cost search / dijkstra """
+    visited = set()
+    cost = {start: 0}
+    parent = {start: None}
+    todo = PriorityQueue()
+  
+    todo.put((0, start))
+    while todo:
+        while not todo.empty():
+            _, vertex = todo.get() # finds lowest cost vertex
+            # loop until we get a fresh vertex
+            if vertex not in visited: 
+                break
+        else: # if todo ran out
+            break # quit main loop
+        visited.add(vertex)
+        if vertex == goal:
+            break
+        for neighbor, distance in G[vertex]:
+            if neighbor in visited: 
+                continue # skip these to save time
+            old_cost = cost.get(neighbor, float('inf')) # default to infinity
+            new_cost = cost[vertex] + distance
+            if new_cost < old_cost:
+                todo.put((new_cost, neighbor))
+                cost[neighbor] = new_cost
+                parent[neighbor] = vertex
+
+    return parent
 
 def closeness_centrality(G, n, p):
     rank = comm.get_rank()
